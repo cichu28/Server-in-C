d94fc3b023745828e42c1d0217e1204a26931c07
Marcin <you@example.com>
2016-11-01 17:13:23 +0000

    "Modyfy - message C->S and S->C, not 100% working"

--- client.c
+++ client.c
@@ -1,59 +1,35 @@
-/****************** CLIENT CODE ****************/
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 
-int main()
-{
-  int clientSocket;
-int newSocket;
-  char buffer[1024];
-
+int main(){
+  int welcomeSocket, clientSocket, newSocket;
+  char buffer1[1024], buffer2[1024];
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
-	signed char PWM_value = 0;
-	char message[30] = "From Client!\n";
-
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  char message[30] = "1\n";
+  struct sockaddr_storage serverStorage;//***
   
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
   serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
   serverAddr.sin_port = htons(8080);
-  /* Set IP address to localhost */
   serverAddr.sin_addr.s_addr = inet_addr("172.24.1.1");
-  /* Set all bits of the padding field to 0 */
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-// *** napisa¢ zabebezpieczenie, ktora chroni, aby wartosc byla 0-100 
-//	printf("Please write PWM value (0-100)\n");
-//	scanf("%i", &PWM_value);
+  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));//***
 
-  /*---- Connect the socket to the server using the address struct ----*/
+//CONNECT
   addr_size = sizeof serverAddr;
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-
-
-
-
-// *** WYSYLANIE
-char buffer_client[1024];
-strcpy(buffer_client,message);
-printf("Message to send: %s\n\n", buffer_client);
-send(newSocket,buffer,11,0);
-
-
-	// *** ODBIERANIE
-  /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
-
-  /*---- Print the received message ----*/
-  printf("Data: %s\n",buffer);   
+//SEND ***
+		strcpy(buffer2,message);
+		send(newSocket,buffer2,13,0);
+  
+  
+//READ
+  recv(clientSocket, buffer2, 1024, 0);
+  printf("S->C: %s",buffer2);   
 
   return 0;
 }
--- server.c
+++ server.c
@@ -1,66 +1,43 @@
-/****************** SERVER CODE ****************/
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
-#include "LedON.h"
 
 int main(){
-	int welcomeSocket, newSocket;
-int clientSocket;
-	char buffer[1024];
-char buffer_client[1024];
+	int welcomeSocket, newSocket, clientSocket;
+	char buffer1[1024], buffer2[1024];
 	struct sockaddr_in serverAddr;
-	struct sockaddr_storage serverStorage;
+	struct sockaddr_storage serverStorage;//***
 	socklen_t addr_size;
-	int counter = 0;
-	//char counter_char;
-	char message[30] = "From Server!!\n";
-	int i=0;
-	signed char PWM_value = 100;
+	char message[30] = "2s\n";
 	init();
 	
-	/*---- Create the socket. The three arguments are: ----*/
-	/* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
 	welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-	 
-	/*---- Configure settings of the server address struct ----*/
-	/* Address family = Internet */
 	serverAddr.sin_family = AF_INET;
-	/* Set port number, using htons function to use proper byte order */
 	serverAddr.sin_port = htons(8080);
-	/* Set IP address to localhost */
 	serverAddr.sin_addr.s_addr = inet_addr("172.24.1.1");
-	/* Set all bits of the padding field to 0 */
 	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-	/*---- Bind the address struct to the socket ----*/
-	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));//***
 
 	while(1)
 	{
-		counter++;
-		/*---- Listen on the socket, with 5 max connection requests queued ----*/
-		if(listen(welcomeSocket,5)==0)		printf("Listening.........\n");
+// LISTEN
+		if(listen(welcomeSocket,5)==0)		printf("Listening\n");
 		else								printf("Error\n");
-
-		/*---- Accept call creates a new socket for the incoming connection ----*/
+//CONNECT
 		addr_size = sizeof serverStorage;
 		newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-
-// *** ODBIERANIE
-recv(clientSocket, buffer_client, 1024, 0);
-printf("Data: %s\n",buffer_client);   
-		/*---- Send message to the socket of the incoming connection ----*/
-
-		// *** WYSYLANIE
-		strcpy(buffer,message);
-		//strcpy(buffer, counter_char);
-		printf("Message %i to send: %s\n", counter, buffer);
-		send(newSocket,buffer,11,0);
-		Led(PWM_value);
+		Led(100);
+		
+//READ***
+  recv(clientSocket, buffer2, 1024, 0);
+  printf("S->C: %s",buffer2); 
+  
+  
+// SEND
+		strcpy(buffer2,message);
+		send(newSocket,buffer2,13,0);
 	}
 
 	return 0;
