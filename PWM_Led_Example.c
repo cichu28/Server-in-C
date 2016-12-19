78b7a608fee479b7d935ca97aa4da431ddd02898
cichu28 <marcincisek@o2.pl>
2016-12-19 19:18:16 +0100

    Update

--- PWM_Led_Example.c
+++ PWM_Led_Example.c
@@ -2,7 +2,7 @@
 #include <softPwm.h>
 
 void init(){
-	wiringPiSetup();                        //                   
+	wiringPiSetup();                        // Incjacja biblioteki do sterowania pinami                  
 	pinMode(0, OUTPUT);                     // Inicjacja pinu numer 0, aby działał jako pin WYJŚIOWY
 	softPwmCreate(0, 0, 10);                // Ustawienie pinu 0, aby działał jako PWM z zakresu 0 do 10
 	digitalWrite(0, LOW);                   // Ustawienie PWM pinu numer 0 na wartość 50
